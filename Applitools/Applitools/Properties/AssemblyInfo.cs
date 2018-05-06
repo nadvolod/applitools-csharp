3b2afaed2d09d2c13a559ab382e0d2bae50d8079
nadvolod <nadvolod@gmail.com>
2018-05-06 13:29:49 -0400

    Fully working applitools test case

---
+++ Applitools/Applitools/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Applitools")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Applitools")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5da685a-6dfd-4ec7-afd3-df340d354152")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Applitools/Applitools/TenMinuteDemo.cs
@@ -0,0 +1,50 @@
+﻿using Applitools.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Applitools
+{
+    [TestFixture]
+    public class TenMinuteDemo
+    {
+        public ChromeDriver Driver { get; private set; }
+        public Eyes Eyes { get; private set; }
+        [SetUp]
+        public void SetupForEverySingleTestMethod()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Eyes = new Eyes
+            {
+                ApiKey = Environment.GetEnvironmentVariable("APPLITOOLS_API_KEY",
+                EnvironmentVariableTarget.User)
+            };
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/fake-landing-page/");
+        }
+        [Test]
+        public void SetBaseline()
+        {
+            Eyes.Open(Driver, "Fake Landing Page", "TC1", new System.Drawing.Size(1024,768));
+            Eyes.CheckWindow();
+        }
+        [Test]
+        public void TestBaseline()
+        {
+            var element = Driver.FindElement(
+                By.XPath("//h1"));
+            var javascript = Driver as IJavaScriptExecutor;
+            javascript.ExecuteScript("arguments[0].setAttribute('style','font-weight:100')", element);
+            Eyes.Open(Driver, "Fake Landing Page", "TC1", new System.Drawing.Size(1024,768));
+            Eyes.CheckWindow();
+        }
+        [TearDown]
+        public void TearDownForEverySingleTestMethod()
+        {
+            Eyes.Close();
+            Driver.Quit();
+            Eyes.AbortIfNotClosed();
+        }
+    }
+}
