576c1ba36cb6a757506255e63ac14fd544dca883
nadvolod <nadvolod@gmail.com>
2018-06-01 07:30:30 -0400

    added comments for all of the code related to match levels

--- Applitools/Applitools/MatchLevels.cs
+++ Applitools/Applitools/MatchLevels.cs
@@ -15,30 +15,44 @@ namespace Applitools
         [Test]
         public void ContentMatchLevel()
         {
+            //Set the applitools match level to Content
             Eyes.MatchLevel = MatchLevel.Content;
+            //Will open the fake pricing page
             GoToPricingPage();
+            //Will change the first element to a Euro symbol instead of the $ 
+            //and will change the collor to a yellow.
             ChangeToEuroAndUpdateColor();
+            //Starts the applitools test with 4 parameters passed in
             Eyes.Open(Driver, AppName, TestCaseName, Resolution1080P);
+            //Validates whether baseline matches the actual image. Passes in extra metadata
+            //about the test case that tells us that this uses MatchLevel.Content
             Eyes.CheckWindow("MatchLevel.Content");
         }
         [Test]
         public void LayoutMatchLevel()
         {
+            //Sets the applitools match leve to Layout
             Eyes.MatchLevel = MatchLevel.Layout;
+            //Will open the fake pricing page
             GoToPricingPage();
+            //Will change the first element to a Euro symbol instead of the $ 
+            //and will change the collor to a yellow.
             ChangeToEuroAndUpdateColor();
+            //Starts the applitools test with 4 parameters passed in
             Eyes.Open(Driver, AppName, TestCaseName, Resolution1080P);
-            Eyes.CheckWindow("MatchLevel.Content");
+            //Validates whether baseline matches the actual image. Passes in extra metadata
+            //about the test case that tells us that this uses MatchLevel.Layout
+            Eyes.CheckWindow("MatchLevel.Layout");
         }
         private void ChangeToEuroAndUpdateColor()
         {
             //take the first element with class name et_pb_sum and update the value to what's specified
             Javascript.ExecuteScript(
                 "document.getElementsByClassName('et_pb_sum')[0].innerText = \"€0\";");
-            var element = Driver.FindElement(
-                By.TagName("h1"));
+            var element = Driver.FindElement(By.TagName("h1"));
+            //Executes some javascript that updates the color of the h1 element on the page to the color specified
             Javascript.ExecuteScript(
-                "arguments[0].setAttribute('style', 'color:#3d72e7!important')",element);
+                "arguments[0].setAttribute('style', 'color:#f9ca33!important')",element);
         }
     }
 }
