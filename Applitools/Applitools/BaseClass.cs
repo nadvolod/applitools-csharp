e07dc90ee0594ae90cf5e4d49406b2860aaf5fe3
nadvolod <nadvolod@gmail.com>
2018-06-03 12:21:33 -0400

    final working solution of ignore regions

--- Applitools/Applitools/BaseClass.cs
+++ Applitools/Applitools/BaseClass.cs
@@ -17,6 +17,11 @@ namespace Applitools
         public string TestCaseName => "Test1";
         public By PriceLocator => By.ClassName("et_pb_sum");
         public By SubheaderLocator => By.XPath("//*[@class='et_pb_text_inner']/p");
+
+        public IWebElement SocialSharingToolbar => Driver.FindElement(
+            By.XPath("//*[@class='et_social_sidebar_networks et_social_visible_sidebar " +
+                     "et_social_slideright et_social_animated et_social_rectangle et_social_sidebar_flip " +
+                     "et_social_sidebar_withcounts et_social_withtotalcount et_social_mobile_on']"));
         
 
         public IJavaScriptExecutor Javascript { get; set; }
@@ -30,11 +35,6 @@ namespace Applitools
         {
             //This uses Selenium to navigate to a url of the page below
             Driver.Navigate().GoToUrl("https://www.ultimateqa.com/fake-pricing-page/");
-        }
-        public void GoToPricingPageWithCurrencyUpdate()
-        {
-            //This uses Selenium to navigate to a url of the page below
-            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/fake-pricing-page-currency/");
         }
         [SetUp]
         public void SetupForEverySingleTestMethod()
--- Applitools/Applitools/IgnoreRegions.cs
+++ Applitools/Applitools/IgnoreRegions.cs
@@ -14,29 +14,69 @@ namespace Applitools
             GoToPricingPage();
             //Will update the subheading text and the currency.
             UpdateSubheadingAndCurrency();
-            //Starts the applitools test with 4 parameters passed in
-            Open1080PBaseline();
         }
         [Test]
         public void IgnoreRegionUsingBy()
         {
+            Eyes.Open(Driver, AppName, "IgnoreRegionUsingBy", Resolution1080P);
             //Ignoring an element with By
-            Eyes.Check(TestCaseName, Target.Window().Ignore(PriceLocator));
+            Eyes.Check("IgnoreRegionUsingBy", Target.Window().Ignore(PriceLocator));
         }
         [Test]
         public void IgnoreRegionUsingFindElements()
         {
+            Eyes.Open(Driver, AppName, "IgnoreRegionUsingFindElements", Resolution1080P);
+
             //Ignoring with FindElement()
-            Eyes.Check(TestCaseName, Target.Window().
+            Eyes.Check("IgnoreRegionUsingFindElements", Target.Window().
                 Ignore(Driver.FindElements(PriceLocator)[1]));
         }
         [Test]
         public void IgnoreMultipleElements()
         {
+            Eyes.Open(Driver, AppName, "IgnoreMultipleElements", Resolution1080P);
 
             //Ignore multiple elements with a single Check()
-            Eyes.Check(TestCaseName, Target.Window().Ignore(PriceLocator, SubheaderLocator));
+            Eyes.Check("IgnoreMultipleElements", Target.Window().Ignore(PriceLocator, SubheaderLocator));
         }
+        [Test]
+        public void FloatingRegion()
+        {
+            Eyes.Open(Driver, AppName, "FloatingRegion", Resolution1080P);
+
+            //This will add a floating region to the social sharing toolbar
+            Eyes.Check("FloatingRegion", Target.Window().Floating(SocialSharingToolbar));
+        }
+        [Test]
+        public void StrictRegion()
+        {
+            Eyes.Open(Driver, AppName, "StrictRegion", Resolution1080P);
+
+            Eyes.MatchLevel = MatchLevel.Layout;
+            //This will add a floating region to the social sharing toolbar
+            Eyes.Check("StrictRegion", Target.Window().Strict(PriceLocator));
+        }
+
+        [Test]
+        public void ContentRegion()
+        {
+            Eyes.Open(Driver, AppName, "ContentRegion", Resolution1080P);
+
+            //This will add a floating region to the social sharing toolbar
+            Eyes.Check("ContentRegion", Target.Window().Content(PriceLocator));
+        }
+        [Test]
+        public void LayoutRegion()
+        {
+            Eyes.Open(Driver, AppName, "LayoutRegion", Resolution1080P);
+
+            //This will add a floating region to the social sharing toolbar
+            Eyes.Check("LayoutRegion", Target.Window().Layout(
+                Driver.FindElement(By.XPath("//*[@class='et_pb_module et_pb_pricing " +
+                                            "clearfix et_pb_pricing_1 et_pb_pricing_tables_0 " +
+                                            "et_pb_no_featured_in_first_row']"))));
+        }
+
         private void UpdateSubheadingAndCurrency()
         {
             var subheadingElement = Driver.FindElement(SubheaderLocator);
