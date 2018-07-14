47beea3fd9ef4fae1e47a8be6bf25b1eed3d2b01
nadvolod <nadvolod@gmail.com>
2018-07-14 09:56:23 -0400

    added comments

--- Applitools/Applitools/Batches.cs
+++ Applitools/Applitools/Batches.cs
@@ -35,19 +35,24 @@ namespace Applitools
 
         private void ScrollToBottomOfPage()
         {
+            //This will scroll to the bottom of the page and wait for 1 second for the action to finish
             Javascript.ExecuteScript("window.scrollTo(0, document.body.scrollHeight)");
             Thread.Sleep(1000);
         }
 
         private void ScrollToTopOfPage()
         {
+            //This will scroll to the top of the page and wait one second
             Javascript.ExecuteScript("window.scrollTo(0, -document.body.scrollHeight)");
+            Thread.Sleep(1000);
         }
 
         private void ClosePopUp()
         {
+            //create a new WebDriverWait object that will poll for 15 seconds for some condition to be true
             var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
             var isClosed = TryToCloseFirstPopUp(wait);
+            //if the first pop up is NOT closed, that means that there was a 2nd pop up that we need to try and close
             if (!isClosed) TryToCloseSecondPopUp(wait);
         }
 
@@ -55,7 +60,7 @@ namespace Applitools
         {
             try
             {
-                wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+                //try to see if the pop up is open and visible for 15 seconds. If it is, click the Close button
                 var popUpCloseButton = wait.Until(
                     ExpectedConditions.ElementIsVisible(By.XPath("//*[name()='svg']")));
                 popUpCloseButton.Click();
@@ -70,6 +75,7 @@ namespace Applitools
         {
             try
             {
+                //try to see if an element with the title below is visible, if it is, click it and return true.
                 var popUpCloseButton = wait.Until(
                     ExpectedConditions.ElementIsVisible(By.XPath("//*[@title='Close']")));
                 popUpCloseButton.Click();
@@ -77,6 +83,7 @@ namespace Applitools
             }
             catch (Exception)
             {
+                //otherwise return false;
                 return false;
             }
         }
@@ -92,12 +99,15 @@ namespace Applitools
         [Test]
         public void GroupingTestSteps()
         {
+            //start a brand new test in Applitools
             Eyes.Open(Driver, AppName, "GroupTestSteps", Resolution1080P);
-
+            //open the ulr below
             Driver.Navigate().GoToUrl("https://www.ultimateqa.com");
+            //visual validation checkpoint that is called HomePage
             Eyes.CheckWindow("HomePage");
-
+            //go to another URL
             Driver.Navigate().GoToUrl("https://www.ultimateqa.com/blog");
+            //2nd step and visual checkpoint called Blog
             Eyes.CheckWindow("Blog");
         }
 
@@ -105,8 +115,10 @@ namespace Applitools
         [Test]
         public void HomePage1080p()
         {
+            //enable full page screenshots so that the entire page is visually validated
             EnableFullPageScreenshots();
             Eyes.Open(Driver, AppName, "1080P", Resolution1080P);
+            //Stitch the entire page so that it can be visually validated
             StitchEntirePageThenCheck(MethodBase.GetCurrentMethod().Name);
         }
 
@@ -119,11 +131,16 @@ namespace Applitools
         }
         private void StitchEntirePageThenCheck(string name = "")
         {
+            //close the pop up offer if it exists
             ClosePopUp();
+            //scroll to the bottom of the page to force all of the dynamic elements to load
             ScrollToBottomOfPage();
+            //scroll back to the top so that the full page screenshot capture starts at the right place
             ScrollToTopOfPage();
             //Eyes.Check(name, Target.Window().Fully());
             Eyes.MatchTimeout = TimeSpan.FromSeconds(3);
+            //perform a visual checkpoint that uses the fluent API to capture a full page screenshot,
+            //add a floating region, add a layout region, add a strict region. All applied at the element level
             Eyes.Check(name,
                 Target.Window().Fully().Floating(SocialSharingToolbar, 180, 4035, 0, 17)
                     .Layout(SocialSharingToolbar)
